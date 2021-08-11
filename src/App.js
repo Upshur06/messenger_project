c1a09064a52de34d19b6cc38cddd7fb9ed1867b1
Upshur06 <mupshur2005@gmail.com>
2021-08-11 07:14:22 -0400

    test button

--- src/App.js
+++ src/App.js
@@ -3,16 +3,25 @@ import { message } from 'statuses';
 import './App.css';
 
 function App() {
+
+  const [input, setInput] = useState('')
   const [messages, setMessages] = useState([
     'testing',
     'test is working',
     'glad it is working'
   ])
+
+  const sendMessageHandler = (e) => {
+      e.preventDefault();
+      setMessages([...messages, input]); 
+      setInput('');
+  } 
+
   return (
     <div className="App">
       <h1>Hello World</h1>
-      <input />
-      <button>Send Message</button>
+      <input vlaue={input} onChange={(e)=> setInput(e.target.value)}/>
+      <button onClick={sendMessageHandler}>Send Message</button>
 
       {
         messages&&

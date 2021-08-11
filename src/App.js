c3f0828fb0736a7f46fa43bd6d205e47400c5c4e
Upshur06 <mupshur2005@gmail.com>
2021-08-11 06:38:02 -0400

    testing useState

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { message } from 'statuses';
 import './App.css';
 
 function App() {
+  const [messages, setMessages] = useState([
+    'testing',
+    'test is working',
+    'glad it is working'
+  ])
   return (
     <div className="App">
       <h1>Hello World</h1>
       <input />
       <button>Send Message</button>
+
+      {
+        messages&&
+        messages.map((message)=>(
+          <p>{message}</p>
+        ))
+      }
     </div>
   );
 }
